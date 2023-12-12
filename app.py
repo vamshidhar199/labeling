cf986823b6d3e958896fa6f904c49c7cc26714f4
vamshidhar199 <vamshiredmi@gmail.com>
2023-12-11 23:08:30 -0800

    new file changes

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import boto3
 from streamlit_img_label import st_img_label
 from streamlit_img_label.manage import ImageManager, ImageDirManager
 from xml.etree import ElementTree as ET
+from PIL import Image
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
 
 def remove_files(directory_path, filename_to_keep):
     for filename in os.listdir(directory_path):
@@ -37,9 +40,89 @@ def fetch_image_and_save_to_folder(directory_path, image_name, save_path):
         image = Image.open(io.BytesIO(image_bytes))
         image.save(os.path.join(save_path, image_name))
         print(f"Image '{image_name}' saved to '{save_path}'")
+        # Check for and download the corresponding TXT file if it exists
+        txt_file_name = image_name.rsplit('.', 1)[0] + '.txt'
+        txt_key = f"ReportedAnnotations/{txt_file_name}"
+        try:
+            txt_object = boto3.client("s3").get_object(Bucket="masterprojectbucket", Key=txt_key)
+            txt_data = txt_object["Body"].read().decode('utf-8')
+
+            # Convert YOLO annotations to XML
+            folder_name = os.path.basename(save_path)
+            image_path = os.path.join(save_path, image_name)
+            xml_data = yolo_to_xml(txt_data, image_name, image.size, custom_labels, folder_name, image_path)
+            xml_save_path = os.path.join(save_path, image_name.rsplit('.', 1)[0] + '.xml')
+            with open(xml_save_path, 'w') as file:
+                file.write(xml_data)
+            print(f"Annotation file '{txt_file_name}' found and converted to XML.")
+        except boto3.client("s3").exceptions.NoSuchKey:
+            print(f"No TXT file found for '{image_name}' in 'ReportedAnnotations'.")
+
+
         remove_files("./img_dir",image_name)
     except Exception as e:
         print(f"Error fetching and saving image: {e}")
+
+def yolo_to_xml(yolo_data, image_name, image_size, class_labels, folder_name, image_path):
+    top = Element('annotation')
+
+    folder = SubElement(top, 'folder')
+    folder.text = folder_name
+
+    filename = SubElement(top, 'filename')
+    filename.text = image_name
+
+    path = SubElement(top, 'path')
+    path.text = image_path
+
+    source = SubElement(top, 'source')
+    database = SubElement(source, 'database')
+    database.text = 'Unknown'
+
+    size_part = SubElement(top, 'size')
+    width = SubElement(size_part, 'width')
+    height = SubElement(size_part, 'height')
+    depth = SubElement(size_part, 'depth')
+    width.text, height.text, depth.text = str(image_size[0]), str(image_size[1]), '3'
+
+    segmented = SubElement(top, 'segmented')
+    segmented.text = '0'
+
+    for line in yolo_data.split('\n'):
+        if line:
+            parts = line.split(',')
+            if len(parts) != 5:
+                continue
+
+            xmin, ymin, xmax, ymax, class_name = parts
+            if class_name not in class_labels:
+                continue
+
+            object_part = SubElement(top, 'object')
+            name = SubElement(object_part, 'name')
+            name.text = class_name
+
+            pose = SubElement(object_part, 'pose')
+            pose.text = 'Unspecified'
+
+            truncated = SubElement(object_part, 'truncated')
+            truncated.text = '0'
+
+            difficult = SubElement(object_part, 'difficult')
+            difficult.text = '0'
+
+            bndbox = SubElement(object_part, 'bndbox')
+            xmin_elem = SubElement(bndbox, 'xmin')
+            ymin_elem = SubElement(bndbox, 'ymin')
+            xmax_elem = SubElement(bndbox, 'xmax')
+            ymax_elem = SubElement(bndbox, 'ymax')
+
+            xmin_elem.text = str(int(float(xmin)))
+            ymin_elem.text = str(int(float(ymin)))
+            xmax_elem.text = str(int(float(xmax)))
+            ymax_elem.text = str(int(float(ymax)))
+
+    return parseString(tostring(top)).toprettyxml(indent="   ")
 
 def run(img_dir, labels):
     print(st.experimental_get_query_params().get('image')[0])
@@ -48,7 +131,7 @@ def run(img_dir, labels):
 
     if "files" not in st.session_state:
         print("inside file not in session")
-        fetch_image_and_save_to_folder("ReportImages", st.experimental_get_query_params().get('image')[0], "img_dir")
+        fetch_image_and_save_to_folder("ReportedRawImages", st.experimental_get_query_params().get('image')[0], "img_dir")
         st.session_state["files"] = idm.get_all_files()
         st.session_state["annotation_files"] = idm.get_exist_annotation_files()
         st.session_state["image_index"] = 0
@@ -105,7 +188,7 @@ def run(img_dir, labels):
             class_name = obj.find('name').text
             class_id = -1
             class_id = class_mapping.index(class_name)
-            print(class_id)
+            #print(class_id)
             if class_id == -1:
                 continue
             
@@ -184,10 +267,23 @@ def run(img_dir, labels):
                 file.write(yolo_annotations)
                 #print(f"YOLO annotations saved to: {output_filepath}")
                 st.success('Annotations saved')
+            
+            # Upload the file to S3
+            s3_client = boto3.client('s3')
+            bucket_name = 'masterprojectbucket'
+            s3_key = f"ReportedAnnotations/{output_filepath.split('/')[-1]}"
+            s3_client.upload_file(output_filepath, bucket_name, s3_key)
+            st.success(f'File uploaded to S3 at {s3_key}')
+            newOutput_filepath = "./img_dir/"+img_file_name.split(".")[0]+".xml"
+            news3_key = f"ReportedAnnotations/{newOutput_filepath.split('/')[-1]}"
+            s3_client.upload_file(output_filepath, bucket_name, news3_key)
+            st.success(f'Xml File uploaded to S3 at {news3_key}')
+            
         except Exception as e:
             st.error('Error saving annotations')
             print(f"Error writing to {output_filepath}: {e}")
         #next_annotate_file()
+        
 
     if rects:
         st.button(label="Save", on_click=annotate)
